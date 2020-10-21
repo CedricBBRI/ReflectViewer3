5758c2d4ecd37b191d3707e05b94e77fc61308f0
CedricBBRI <72795350+CedricBBRI@users.noreply.github.com>
2020-10-21 13:35:02 +0200

    TexturePopup0_1

--- Assets/Custom Reflect Viewer/Scripts/ChangeMaterial.cs
+++ Assets/Custom Reflect Viewer/Scripts/ChangeMaterial.cs
@@ -16,6 +16,12 @@ namespace UnityEngine.Reflect
         public Material newMaterial;
         Material newMaterialCopy;
         public Image newMaterialCopyImage;
+
+        public Text floatName;
+        public Camera mainCam;
+        public float[] floatNameOffset;
+        public float[] floatImgOffset;
+        private Vector3 hitPoint;
         
 
         GameObject root;// = GameObject.Find("Root");
@@ -40,6 +46,10 @@ namespace UnityEngine.Reflect
         {
             if (toggle.isOn)
             {
+                if (selectedObject != null)
+                {
+                    CreateUI(selectedObject);
+                }
                 // instead of GameObject, could use custom type like ControllableUnit
                 if (Input.GetMouseButtonUp(0)) //left click
                 {
@@ -50,24 +60,25 @@ namespace UnityEngine.Reflect
                     string selectedCostString = selectedMeta.GetParameter("Cost");
                     //float selectedCost = float.Parse(selectedMeta.GetParameter("Cost"));
                     showText.text = selectedObject.name + " with cost: " + selectedCostString;
-                    newMaterialCopy = selectedObject.GetComponent<Renderer>().material;
+                    newMaterialCopy = new Material(selectedObject.GetComponent<Renderer>().material);
+                    newMaterialCopy.shader = Shader.Find("Unlit/Texture");
                     newMaterialCopyImage.material = newMaterialCopy;
                 }
                 if (Input.GetMouseButtonUp(1) && Input.GetKey(KeyCode.LeftControl)) //right click
                 {
-                    selectedObject = clickObjects();
-                    Debug.Log(selectedObject.name);
-                    var selectedMeta = selectedObject.GetComponent<Metadata>();
+                    GameObject selectedObjectLocal = clickObjects();
+                    Debug.Log(selectedObjectLocal.name);
+                    var selectedMeta = selectedObjectLocal.GetComponent<Metadata>();
                     string selectedCostString = selectedMeta.GetParameter("Cost");
                     //float selectedCost = float.Parse(selectedMeta.GetParameter("Cost"));
                     //showText.text = selectedObject.name + " with cost: " + selectedCostString;
                     if(Input.GetKey(KeyCode.LeftAlt))
                     {
-                        selectedObject.GetComponent<MeshRenderer>().material = newMaterialCopy;
+                        selectedObjectLocal.GetComponent<MeshRenderer>().material = newMaterialCopy;
                     }
                     else
                     {
-                        selectedObject.GetComponent<MeshRenderer>().material = newMaterial;
+                        selectedObjectLocal.GetComponent<MeshRenderer>().material = newMaterial;
                     }
                 }
             }
@@ -78,17 +89,42 @@ namespace UnityEngine.Reflect
             GameObject target = null;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            //Debug.Log("Shooting.");
             if (Physics.Raycast(ray, out hit)) // you can also only accept hits to some layer and put your selectable units in this layer
             {
-                //Debug.Log("Shooting.2");
                 if (hit.transform != null && hit.transform.IsChildOf(root.transform))
                 {
-                    target = hit.transform.gameObject; 
-                    //Debug.Log(target.name);
+                    target = hit.transform.gameObject;
+                    hitPoint = hit.point;// - ray.GetPoint(0.01f);
                 }
             }
             return target;
+        }
+
+        public void CreateUI(GameObject go)
+        {
+            Vector3 nameOffset = new Vector3();
+            nameOffset.x = floatNameOffset[0];
+            nameOffset.y = floatNameOffset[1];
+            nameOffset.z = floatNameOffset[2];
+            Vector3 imOffset = new Vector3(floatImgOffset[0], floatImgOffset[1], floatImgOffset[2]);
+            floatName.text = go.name;
+            floatName.transform.position = mainCam.WorldToScreenPoint(hitPoint) + nameOffset;
+            newMaterialCopyImage.transform.position = mainCam.WorldToScreenPoint(hitPoint) + imOffset;
+            //newMaterialCopy.shader = ;
+            Material tempMat = new Material(newMaterial);
+            newMaterialCopyImage.material = tempMat;
+            tempMat.shader = Shader.Find("Unlit/Texture");
+            newMaterialCopyImage.GetComponent<Button>().onClick.AddListener(() => OnClick(newMaterial));
+        }
+
+        public void ChangeMaterialClick(Material mat)
+        {
+            selectedObject.GetComponent<MeshRenderer>().material = mat;
+        }
+
+        public void OnClick(Material mat)
+        {
+            ChangeMaterialClick(mat);
         }
     }
 }
