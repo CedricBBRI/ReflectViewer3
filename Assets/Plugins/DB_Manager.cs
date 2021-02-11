91838fd46297a355ca35c1d86ccc55bd056ff7be
CedricBBRI <72795350+CedricBBRI@users.noreply.github.com>
2021-02-11 16:12:35 +0100

    MySqlStart

---
+++ Assets/Plugins/DB_Manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+using System;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using UnityEngine.SceneManagement;
+
+public class DB_Manager : MonoBehaviour
+{
+    public static DB_Manager instance;
+    public string host,database,username,password;
+    MySqlConnection con;
+
+    private void Start()
+    {
+
+    }
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Connect_DBB()
+    {
+
+    }
+
+}
