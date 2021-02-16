40bb9c0d946aecb168b2ec78652ae3f6f6354104
CedricBBRI <72795350+CedricBBRI@users.noreply.github.com>
2021-02-16 11:29:26 +0100

    MySQLimplemented

--- Assets/Plugins/DB_Manager.cs
+++ Assets/Plugins/DB_Manager.cs
@@ -15,7 +15,10 @@ public class DB_Manager : MonoBehaviour
 
     private void Start()
     {
+        Connect_BDD();
 
+        string strOutput = LeaderBoard(10);
+        Debug.Log("DBtest: " + strOutput);
     }
     void Awake()
     {
@@ -30,9 +33,45 @@ public class DB_Manager : MonoBehaviour
         }
     }
 
-    void Connect_DBB()
+    void Connect_BDD()
     {
+        String cmd = "SERVER=" + host + ";" + "database =" + database + ";User ID=" + username + ";Password=" + password + ";Pooling=true;Charset=utf8;" ;
+        try
+        {
+            con = new MySqlConnection(cmd);
+            con.Open();
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.ToString());
+        }
+    }
 
+    private void Update()
+    {
+        //Debug.Log(con.State);
+    }
+
+    public string LeaderBoard(int limit)
+    {
+        try
+        {
+            //Connect_BDD();
+            MySqlCommand cmdSql = new MySqlCommand("SELECT * FROM `Tiles` ORDER BY `Consommation 12 derniers mois` DESC LIMIT " + limit, con);
+            MySqlDataReader myReader = cmdSql.ExecuteReader();
+
+            string data = null;
+            while (myReader.Read())
+            {
+                data += myReader["Code Art"].ToString() + ": " + myReader["Consommation 12 derniers mois"] + "\n";
+            }
+            myReader.Close();
+            return data;
+        }
+        catch
+        {
+            return null;
+        }
     }
 
 }
